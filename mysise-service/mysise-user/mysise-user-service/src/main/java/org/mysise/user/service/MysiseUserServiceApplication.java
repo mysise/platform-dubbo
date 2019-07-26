c3172453f190ce06813cfa1c07f0de341f6fb2fb
fanwenjie <892662026@qq.com>
2019-07-26 23:06:47 +0800

    user-service-service

---
+++ mysise-service/mysise-user/mysise-user-service/src/main/java/org/mysise/user/service/MysiseUserServiceApplication.java
@@ -0,0 +1,13 @@
+package org.mysise.user.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysiseUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysiseUserServiceApplication.class, args);
+    }
+
+}
---
+++ mysise-service/mysise-user/mysise-user-service/src/test/java/org/mysise/user/service/MysiseUserServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package org.mysise.user.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MysiseUserServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
