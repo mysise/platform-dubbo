29b8d4f9fb129bed2f683e8127abb269dda69fd8
fanwenjie <892662026@qq.com>
2019-07-28 15:58:25 +0800

    log4j2

--- mysise-service/mysise-user/mysise-user-service/src/test/java/org/mysise/user/service/MysiseUserServiceApplicationTests.java
+++ mysise-service/mysise-user/mysise-user-service/src/test/java/org/mysise/user/service/MysiseUserServiceApplicationTests.java
@@ -1,16 +1,27 @@
 package org.mysise.user.service;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mysise.user.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MysiseUserServiceApplicationTests {
 
+    @Autowired
+    private UserService userService;
+
     @Test
     public void contextLoads() {
+
+        List<User> list =  userService.listUser();
+        Assert.assertNotNull(list);
     }
 
 }
