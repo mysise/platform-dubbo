7795b7d6d1e38e880831266d3f75e5e673ed15c9
fanwenjie <892662026@qq.com>
2019-08-23 00:45:59 +0800

    service 扫描

--- mysise-service/mysise-demo1/mysise-demo1-service/src/main/java/org/mysise/demo1/service/Demo1ServiceImpl.java
+++ mysise-service/mysise-demo1/mysise-demo1-service/src/main/java/org/mysise/demo1/service/Demo1ServiceImpl.java
@@ -5,10 +5,12 @@ import org.mysise.common.base.FeedResult;
 import org.mysise.demo1.api.Demo1Service;
 import org.mysise.demo1.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
 
+@Component
 @Service(version = "1.0.0",interfaceClass = Demo1Service.class,filter = "tracing")
 public class Demo1ServiceImpl implements Demo1Service {
 
